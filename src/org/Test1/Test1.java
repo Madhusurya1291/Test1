1b5201febb92e60c37af2671629dd4a1a4e1f17a
madhusurya1291 <madhusurya1116@gmail.com>
2022-08-17 13:38:34 +0530

    Project added

---
+++ src/org/Test1/Test1.java
@@ -0,0 +1,16 @@
+package org.Test1;
+
+public class Test1 {
+
+	public static void main(String[] args)
+	{
+		String hungry="Yes";
+		if(hungry=="Yes")
+		{
+			System.out.println("Eat");
+		}
+		else
+			System.out.println("Do work");
+	}
+
+}
